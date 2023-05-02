6524c481f581505938ec5067d048dcd836cec1c9
Thiago Vasconcelos <pedrosav14@gmail.com>
2023-05-01 21:47:56 -0300

    ref: dot product functions is part of the class now and sigmoide_derivative was refactored

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -1,6 +1,5 @@
-from math import tanh
+from math import tanh, exp
 from typing import List
-from math import exp
 
 
 class ActivationFunctions:
@@ -13,13 +12,11 @@ class ActivationFunctions:
 
     @staticmethod
     def sigmoid_function(self) -> float:
-        # output range (0 to 1)
-        return 1 / (1 + exp(- self.n))
+        return 1 / (1 + exp(-self.n))
 
     @staticmethod
     def sigmoid_derivation(self) -> float:
-        sigmoid = ActivationFunctions.sigmoid_function(self.n)
-        return sigmoid * (1 - sigmoid)
+        return self.sigmoid_function(self.n) * (1 - self.sigmoid_function(self.n))
 
     @staticmethod
     def binary_step(self) -> int:
@@ -35,18 +32,18 @@ class ActivationFunctions:
     def hiperbolic_derivative(self) -> float:
         return 1 - tanh(self.n) ** 2
 
+    @staticmethod
+    def dot_product(vector_x: List[float], vector_y: List[float]) -> float:
+        result = 0.0
+        len_x = len(vector_x)
+        len_y = len(vector_y)
 
-def dot_product(vector_x: List[float], vector_y: List[float]) -> float:
-    result = 0.0
-    len_x = len(vector_x)
-    len_y = len(vector_y)
+        if len_x > len_y:
+            vector_y.extend([0.0] * (len_x - len_y))
+        elif len_y > len_x:
+            vector_x.extend([0.0] * (len_y - len_x))
 
-    if len_x > len_y:
-        vector_y.extend([0.0] * (len_x - len_y))
-    elif len_y > len_x:
-        vector_x.extend([0.0] * (len_y - len_x))
+        for i in range(len(vector_x)):
+            result += vector_x[i] * vector_y[i]
 
-    for i in range(len(vector_x)):
-        result += vector_x[i] * vector_y[i]
-
-    return result
+        return result
