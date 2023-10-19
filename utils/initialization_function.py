79ceeaf4643598416bd99fd3a0d293e7c01646f2
Thiago Vasconcelos <pedrosav14@gmail.com>
2023-10-18 22:02:08 -0300

    feat: Xavier and Kaiming(Normal and Uniform distribution) initializations

--- utils/initialization_function.py
+++ utils/initialization_function.py
@@ -1,3 +1,4 @@
+from typing import List
 import numpy as np
 
 
@@ -7,9 +8,38 @@ class InitializationFunction:
         self.num_outputs = num_outputs
 
     @staticmethod
-    def xavier_initialization(self):
-        limit = (6 / (self.num_inputs + self.num_outputs)) ** 0.5
+    def uniform_xavier_initialization(self) -> List[float]:
+        limit = (6 / (self.num_inputs + self.num_outputs)) ** (1/2)
         weights = []
         for i in range(self.num_outputs):
-            weights = np.random.uniform(-limit, limit, size=(self.num_inputs, self.num_outputs))
+            weights = np.random.uniform(low=-limit, high=limit, size=(self.num_inputs, self.num_outputs))
+
+        return weights
+
+    @staticmethod
+    def normal_xavier_initialization(self) -> List[float]:
+        limit = (2 / self.num_inputs + self.num_outputs) ** (1/2)
+        weights: list = []
+        for i in range(self.num_outputs):
+            weights.append([np.random.normal(loc=0.0, scale=limit, ) for _ in range(self.num_inputs)])
+
+        return weights
+
+    @staticmethod
+    def uniform_kaiming_initialization(self) -> List[float]:
+        # Typically used when the activation function was ReLU or PReLU
+        limit = (6 / self.num_inputs) ** (1/2)
+        weights = []
+        for i in range(self.num_outputs):
+            weights = np.random.uniform(low=-limit, high=limit, size=(self.num_inputs, self.num_outputs))
+
+        return weights
+
+    @staticmethod
+    def normal_kaiming_initialization(self) -> List[float]:
+        limit = (2 / self.num_inputs) ** (1 / 2)
+        weights = []
+        for i in range(self.num_outputs):
+            weights = np.random.uniform(low=0.0, high=limit, size=(self.num_inputs, self.num_outputs))
+
         return weights
