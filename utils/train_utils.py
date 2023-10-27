29ed7db82e255994cea6a895651bd964b85e5907
Thiago Vasconcelos <pedrosav14@gmail.com>
2023-10-26 21:15:06 -0300

    ref: Changed type hint and added round to some implementation

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -1,27 +1,30 @@
 from math import tanh, exp
+from typing import List
 
 
 class ActivationFunction:
-    def __init__(self, vector: list[float]):
+    def __init__(self, vector: List[float]):
         self.vector = vector
 
     @staticmethod
-    def linear_function(self) -> list[float]:
+    def linear_function(self) -> List[float]:
         return self.vector
 
     @staticmethod
-    def sigmoid_function(self) -> list[float]:
-        for _ in range(len(self.vector)):
-            self.vector[_] = 1 / (1 + exp(-self.vector[_]))
-
-        return self.vector
+    def sigmoid(self) -> List[float]:
+        return [round(1 / (1 + exp(-self.vector[_])), 4) for _ in range(len(self.vector))]
 
     @staticmethod
-    def sigmoid_derivation(self) -> float:
-        return self.sigmoid_function(self.vector) * (1 - self.sigmoid_function(self.vector))
+    def sigmoid_derivative(self) -> List[float]:
+        derivatives = []
+        for val in self.vector:
+            sigmoid_val = 1 / (1 + exp(-val))
+            derivatives.append((sigmoid_val * (1 - sigmoid_val)).__round__(4))
+
+        return derivatives
 
     @staticmethod
-    def binary_step(self) -> list[float]:
+    def binary_step(self) -> List[float]:
         # output (0 or 1)
         for _ in range(len(self.vector)):
             if self.vector[_] >= 0:
@@ -32,17 +35,16 @@ class ActivationFunction:
         return self.vector
 
     @staticmethod
-    def hiperbolic_tangent(self) -> float:
+    def hiperbolic_tangent(self) -> List[float]:
         # output range (-1 to 1)
-        return (exp(self.vector) - exp(-self.vector)) / (exp(self.vector) + exp(-self.vector))
+        return [round(tanh(self.vector[_]), 4) for _ in range(len(self.vector))]
 
     @staticmethod
-    def hiperbolic_derivative(self) -> list[float]:
-        # return 1 - tanh(self.vector) ** 2
-        return self.vector
+    def hiperbolic_derivative(self) -> List[float]:
+        return [round(1 - tanh(self.vector[_]) ** 2, 4) for _ in range(len(self.vector))]
 
     @staticmethod
-    def relu(self) -> list[float]:
+    def relu(self) -> List[float]:
         # output (0, n)
         for _ in range(len(self.vector)):
             if self.vector[_] < 0:
@@ -51,7 +53,7 @@ class ActivationFunction:
         return self.vector
 
     @staticmethod
-    def relu_derivative(self) -> list[float]:
+    def relu_derivative(self) -> List[float]:
         # output (0,1)
         for _ in range(len(self.vector)):
             if self.vector[_] <= 0:
@@ -60,7 +62,7 @@ class ActivationFunction:
         return self.vector
 
     @staticmethod
-    def dot_product(vector_x: list[float], vector_y: list[float]) -> float:
+    def dot_product(vector_x: List[float], vector_y: List[float]) -> float:
         result = 0.0
         len_x = len(vector_x)
         len_y = len(vector_y)
